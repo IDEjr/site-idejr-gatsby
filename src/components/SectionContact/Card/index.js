c8b39b3da405d3d24634e419e39852959552ccca
nikolas <ncchemale@idejr.com.br>
2020-11-07 13:32:50 -0300

    add titulo e estrutura dos forms

---
+++ src/components/SectionContact/Card/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+import "./style.css"
+
+
+export default function Contact() {
+    return (
+       <div className="container">
+         <div className="section-title">Quer falar conosco ?</div>
+         <div className="flex-form">
+             <div>ola</div>
+             <div>ola</div>
+             <div>ola</div>
+             <div>ola</div>
+             <div>ola</div>
+         </div>
+       </div>
+    );
+  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from "react"
 
 // Section Import 
 import SectionProdutos from "../components/SectionProducts"
+import SectionContact from "../components/SectionContact/Card/index"
 
 const IndexPage = () => (
   <>
+  <SectionContact/>
   </>
 )
 
