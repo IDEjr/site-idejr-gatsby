fa09df163b9e05a7cc1c139cf5a54ed9a0b53a6c
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-11-09 19:58:18 -0300

    Add section Clientes Index

---
+++ src/components/SectionClientes/index.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import Slider from 'infinite-react-carousel';
+
+// Components
+import SectionContainer from '../shared/SectionContainer';
+
+// Suport
+import { viewportIsSmall } from '../../support/responsiveness'
+
+const itensCarousel = [
+  { image:"https://www.ufpb.br/educacaofinanceira/contents/imagens/brasoes-universidades/ufrgs.png/@@images/image.png", link:"https://idejr.com.br/", empresa:"IDE" },
+  { image:"https://www.ufpb.br/educacaofinanceira/contents/imagens/brasoes-universidades/ufrgs.png/@@images/image.png", link:"https://idejr.com.br/", empresa:"IDE" },
+  { image:"https://www.ufpb.br/educacaofinanceira/contents/imagens/brasoes-universidades/ufrgs.png/@@images/image.png", link:"https://idejr.com.br/", empresa:"IDE" },
+  { image:"https://www.ufpb.br/educacaofinanceira/contents/imagens/brasoes-universidades/ufrgs.png/@@images/image.png", link:"https://idejr.com.br/", empresa:"IDE" },
+  { image:"https://www.ufpb.br/educacaofinanceira/contents/imagens/brasoes-universidades/ufrgs.png/@@images/image.png", link:"https://idejr.com.br/", empresa:"IDE" },
+  { image:"https://www.ufpb.br/educacaofinanceira/contents/imagens/brasoes-universidades/ufrgs.png/@@images/image.png", link:"https://idejr.com.br/", empresa:"IDE" },
+  { image:"https://www.ufpb.br/educacaofinanceira/contents/imagens/brasoes-universidades/ufrgs.png/@@images/image.png", link:"https://idejr.com.br/", empresa:"IDE" },
+
+]
+
+const PartnerSectionCarouselSlide = styled.div`
+a {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+a > img {
+  max-height: 120px;
+  margin: 0;
+}
+`
+
+const Container = styled(SectionContainer)`
+    padding: 0px;
+`
+
+export default class SectionClientes extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      carouselSettings: {
+        arrows: false,
+        arrowsBlock: false,
+        autoplay: true,
+        slidesToShow: this.getHowManySlidesToShow(),
+        className: "teste",
+        pauseOnHover: true,
+        centerMode:	true,
+      }
+    }
+  }
+
+  updateCarouselSettings(newSettings) {
+    this.setState({
+      ...this.state,
+      carouselSettings: {
+        ...this.state.carouselSettings,
+        ...newSettings
+      }
+    })
+  }
+
+  getHowManySlidesToShow() {
+    if(viewportIsSmall()) {
+      return 2
+    }
+    return 5
+  }
+
+  updateSettingsIfBreakpointReached(){
+    this.updateCarouselSettings({
+      slidesToShow: this.getHowManySlidesToShow()
+    })
+  }
+  
+  render() {
+
+    return (
+        <Container>
+            <Slider { ...this.state.carouselSettings } onResize={() => this.updateSettingsIfBreakpointReached()}>
+            {itensCarousel.map(function (item, index) { return (
+
+                <PartnerSectionCarouselSlide key={index}>
+
+                <a href={item.link} target="_blank" rel="noopener noreferrer" >
+                    <img src={item.image} className='image' alt={item.empresa} />
+                </a>
+
+                </PartnerSectionCarouselSlide>
+
+            )})}
+            </Slider>    
+        </Container>
+    ) 
+  }
+}
