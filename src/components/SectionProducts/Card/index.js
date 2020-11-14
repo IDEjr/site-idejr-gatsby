8beab38834eda78c0980c332ab75ba51453524df
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-11-14 20:45:11 -0300

    Fix merge

--- src/components/SectionProducts/Card/index.js
+++ src/components/SectionProducts/Card/index.js
@@ -1,24 +1,5 @@
 import React from "react"
 
-<<<<<<< HEAD
-// Images
-import Institucional from "../../../assets/product-icon-institucional.png"
-
-// Styled Components
-import {CardContainer,ImageContainer,Dot} from "./styles"
-
-const Card = () => {
-
-    return(
-        <CardContainer>
-            <ImageContainer>
-                <img src={Institucional} alt="Institucional" />
-                <h3>Site Institucional</h3>
-                <Dot/>
-                <h4>Ola meu nome é felopildo</h4>
-            </ImageContainer>
-        </CardContainer>
-=======
 // Styled Components
 import {GridContainer,CardContainer,ImageContainer,Dot} from "./styles"
 
@@ -34,7 +15,6 @@ const Card = ({Image,Title,Description}) => {
                 <h4>{Description}</h4>
             </CardContainer>
         </GridContainer>    
->>>>>>> section/produtos
     )
 }
 
--- src/components/SectionProducts/Card/styles.js
+++ src/components/SectionProducts/Card/styles.js
@@ -4,12 +4,6 @@ import Colors from "../../../style/colors"
 // Styled Components
 import styled from "styled-components"
 
-<<<<<<< HEAD
-export const CardContainer = styled.div`
-    height: 400px;
-    width: 300px;
-    position: relative;
-=======
 export const GridContainer = styled.div`
     display: flex;
     width: 100%;
@@ -34,26 +28,17 @@ export const CardContainer = styled.div`
 
     position: relative;
     text-align: center;
->>>>>>> section/produtos
 
     border-style: solid;
     border-radius: 15px;
     border-color: ${Colors.PURPLE};
-<<<<<<< HEAD
-    border-width: 3px;
-=======
     border-width: 2px;
->>>>>>> section/produtos
 
     background-color: #ffffff;
     font-family: 'Poppins', sans-serif;
 
     h3 {
-<<<<<<< HEAD
-        font-weight: bold;
-=======
         font-weight: 900;
->>>>>>> section/produtos
         font-size: 1.3rem;
         color: ${Colors.PURPLE};
         margin-bottom: 10px;
@@ -62,25 +47,15 @@ export const CardContainer = styled.div`
         font-weight: 300;
         font-size: 1rem;
         color: ${Colors.BLACK};
-<<<<<<< HEAD
-    }
-    &:nth-child(2n+1){
-        left: 0px;
-=======
         text-align: justify;
         text-align-last: left;
         margin-bottom: 0px;
->>>>>>> section/produtos
     }
 `;
 
 export const ImageContainer = styled.div`
     height: 160px;
-<<<<<<< HEAD
-    margin: 50px 50px 0px 50px;    
-=======
     padding: 5% 15% 0px 15%;    
->>>>>>> section/produtos
     background-image: Institucional;
 
     img {
