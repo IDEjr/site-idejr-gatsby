7b870804aff448b7f322a501327cd379a9c042dd
nikname22 <63079898+nikname22@users.noreply.github.com>
2020-11-24 19:37:36 -0300

    Section/hero (#17)
    * posicionando elementos
    
    * finalização parcial sem background
    
    * add styled components
    
    * add Banner
    
    * formatação do texto da section
    
    Co-authored-by: nikolas <ncchemale@idejr.com.br>

---
+++ src/components/SectionHero/Card/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+import {Container, TextContainer, Text, SubText} from "./style.js";
+
+
+
+export default function Hero() {
+    return (
+        <Container>
+            <TextContainer>
+                <Text>
+                    Experiencia em 
+                    desenvolvimento web &
+                    sistemas sob encomenda
+                </Text>
+                <SubText>
+                    Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna 
+                    aliqua.                  
+                </SubText>
+            </TextContainer>
+        </Container>
+    );
+}
---
+++ src/components/SectionHero/Card/style.js
@@ -0,0 +1,52 @@
+import styled from "styled-components"
+import Banner from "../../../images/hero-background.jpg"
+
+export const Container = styled.div`
+    background-image: url(${Banner});
+    width:100%;
+    height: 95vh;     
+    display:flex;
+    flex-direction: column;
+    align-items: flex-start;  
+    justify-content: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    font-family: 'Poppins', sans-serif;    
+ `;
+
+export const TextContainer = styled.div`    
+    width: 300px;       
+    margin:5%;
+    padding:10px;    
+
+  @media (min-width:760px) {
+   width: 600px;
+   height: 200px;
+   margin: 10%;
+                    
+}
+`;
+
+export const Text = styled.div`
+    font-size: 25px;    
+    color: white;
+    font-weight:bold;
+    
+
+  @media (min-width:1020px) {
+    font-size: 40px;
+    color: white;
+    line-height: 1.0;             
+}
+
+`;
+
+export const SubText = styled.h4`
+    color: white;
+    margin-top: 20px;
+    font-weight:300;
+
+  @media (min-width:1020px) {    
+    margin-top: 20px;             
+}
+`;
