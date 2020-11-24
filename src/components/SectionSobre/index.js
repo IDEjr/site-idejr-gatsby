8f095d01fc3056d2c8a363f550ab9b7e9cfc40a8
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-11-24 19:03:31 -0300

    Add SectionSobre Text

--- src/components/SectionSobre/index.js
+++ src/components/SectionSobre/index.js
@@ -2,18 +2,27 @@ import React from "react"
 
 // Components
 import SectionContainer from "../shared/SectionContainer"
+import Center from "../shared/Center"
+import SectionTitle from "../SectionTitle"
 
 // Styled Components
-import {LinksContainer, TextContainer} from "./style"
+import {LinksContainer, TextContainer, Bar, Text} from "./style"
 
 const SectionSobre = () => {
     return(
         <SectionContainer>
             <LinksContainer>
-                Aada
+                
             </LinksContainer>
             <TextContainer>
-                ASD
+                    <Center>
+                        <SectionTitle uppertitle={"Quem somos"} title={"Nós te ajudamos a colocar seu negócio na era digital"}/>
+                    </Center>
+                    <Text>
+                        <Bar />
+                        A IDE é a empresa júnior dos cursos Ciência e Engenharia de Computação da Universidade Federal do Rio Grande do Sul.<br/><br/>
+                        Formada por alunos interessados em se tornarem empreendedores e profissionais excepcionais, a IDE fornece soluções em software, como sites e sistemas, com preços abaixo do mercado aliados à excelência da UFRGS, o Instituto de Informática e seus professores, que fornecem apoio técnico e aconselhamento aos estudantes.
+                    </Text>
             </TextContainer>
         </SectionContainer>
     )
--- src/components/SectionSobre/style.js
+++ src/components/SectionSobre/style.js
@@ -1,6 +1,9 @@
 // Styled Components
 import styled from "styled-components"
 
+// Colors
+import Colors from "../../style/colors"
+
 export const LinksContainer = styled.div`
     
     width: 50px;
@@ -14,11 +17,39 @@ export const LinksContainer = styled.div`
 
 export const TextContainer = styled.div`
     
-    width: 400px;
+    width: 600px;
     height: 100%;
 
     @media screen and (max-width: 760px) { 
         &{
+            width: 100%;
         }
     } 
+`;
+
+export const Text = styled.div`
+
+    display: flex;
+    flex-direction: row;
+    text-align: justify;
+     text-justify: inter-word;
+
+    padding-right: 100px;
+
+    @media screen and (max-width: 760px) { 
+        &{
+            flex-direction: column;
+            padding-right: 0px;
+        }
+    } 
+`;
+
+export const Bar = styled.div`
+
+    background-color: ${Colors.BLUE};
+    height: 1px;
+    width: 70px;
+    min-width: 70px;
+    margin-bottom: 30px;
+    margin-right: 30px;
 `;
