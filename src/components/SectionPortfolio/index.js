93fad709f6b1ccb6228a19468c6b91f45d4faff6
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2021-03-05 16:01:32 -0300

    Add novas imagens no portfolio

--- src/components/SectionPortfolio/index.js
+++ src/components/SectionPortfolio/index.js
@@ -3,10 +3,15 @@ import Slider from 'infinite-react-carousel';
 import { SectionContainer, SectionBackground, SliderContainer, Control } from './style'
 import PortfolioItem from './PortfolioItem'
 import SectionHeader from '../shared/SectionHeader'
+
+//Images
 import Blob from '@assets/bg-blob-2.svg'
 import item1Logo from '@assets/portfolio-item-1-logo.png'
 import item1Content from '@assets/portfolio-item-1-content.png'
-// Components
+import item2Logo from '@assets/portfolio-item-2-logo.png'
+import item2Content from '@assets/portfolio-item-2-content.png'
+import item3Logo from '@assets/portfolio-item-3-logo.png'
+import item3Content from '@assets/portfolio-item-3-content.png'
 
 export default function SectionPortfolio() {
   return (
@@ -20,8 +25,12 @@ export default function SectionPortfolio() {
             contentProps={{src: item1Content, alt: "amostra website ceanut"}}
           />
           <PortfolioItem
-            logoProps={{src: item1Logo, alt: "logo ceanut"}}
-            contentProps={{src: item1Content, alt: "amostra website ceanut"}}
+            logoProps={{src: item2Logo, alt: "logo nejpoa"}}
+            contentProps={{src: item2Content, alt: "amostra website nejpoa"}}
+          />
+          <PortfolioItem
+            logoProps={{src: item3Logo, alt: "logo RS Racing"}}
+            contentProps={{src: item3Content, alt: "amostra website RS Racing"}}
           />
         </Slider>
       </SliderContainer>
